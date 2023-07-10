21f70e9f948de4864f0c4e56149a629aa68dc8be
Andy <53950662+AdrianRazor@users.noreply.github.com>
2023-07-10 10:48:24 +0300

    added post-team section

--- src/js/main.js
+++ src/js/main.js
@@ -48,50 +48,62 @@ document.addEventListener("DOMContentLoaded", function (event) {
   }
 
   // Splide
-  if (document.querySelector(".splidePrograms")) {
-    var splidePrograms = new Splide(".splidePrograms", {
-      type: "loop",
-      autoWidth: true,
-      speed: 600,
-      perPage: 3,
-      gap: 20,
-      focus: "center",
-      arrowPath:
-        "M19,27.05a.82.82,0,0,0,.59-.26l6-6.49a.78.78,0,0,0,.15-.22v0h0a1.69,1.69,0,0,0,.05-.24h0v-.06a.77.77,0,0,0-.06-.3h0l0-.06a1.43,1.43,0,0,0-.14-.2h0l-6-6a.81.81,0,0,0-1.14,1.14L23.07,19H15a.8.8,0,0,0,0,1.6h8.17l-4.76,5.16A.8.8,0,0,0,19,27.05Z",
-      pagination: false,
-      updateOnMove: true,
-      breakpoints: {
-        1199: {
-          perPage: 1,
-        },
-        767: {
-          type: "slide",
-          autoWidth: false,
-          focus: false,
-        },
+  const splideOptions = {
+    type: "loop",
+    autoWidth: true,
+    speed: 600,
+    perPage: 3,
+    gap: 20,
+    focus: "center",
+    arrowPath:
+      "M19,27.05a.82.82,0,0,0,.59-.26l6-6.49a.78.78,0,0,0,.15-.22v0h0a1.69,1.69,0,0,0,.05-.24h0v-.06a.77.77,0,0,0-.06-.3h0l0-.06a1.43,1.43,0,0,0-.14-.2h0l-6-6a.81.81,0,0,0-1.14,1.14L23.07,19H15a.8.8,0,0,0,0,1.6h8.17l-4.76,5.16A.8.8,0,0,0,19,27.05Z",
+    pagination: false,
+    updateOnMove: true,
+    breakpoints: {
+      1199: {
+        perPage: 1,
       },
-    });
+      767: {
+        type: "slide",
+        autoWidth: false,
+        focus: false,
+      },
+    },
+  };
+
+  if (document.querySelector(".splidePrograms")) {
+    var splidePrograms = new Splide(".splidePrograms", splideOptions);
     splidePrograms.mount();
   }
+  splideProgressBar(splidePrograms);
 
-  // Set default width of progress bar
-  const progressBar = splidePrograms.root.querySelector(
-    ".splide__progress-bar"
-  );
-  let progressBarWidth = 100 / splidePrograms.length;
-  progressBar.style.width = `${progressBarWidth}%`;
+  if (document.querySelector(".splidePostTeam")) {
+    var splidePostTeam = new Splide(".splidePostTeam", {
+      ...splideOptions,
+      gap: 80,
+    });
+    splidePostTeam.mount();
+  }
+  splideProgressBar(splidePostTeam);
 
-  // Progress bar on slider moving
-  splidePrograms.on("mounted move", function () {
-    var end = splidePrograms.Components.Controller.getEnd() + 1;
-    var rate = Math.min((splidePrograms.index + 1) / end, 1);
-    progressBar.style.width = String(100 * rate) + "%";
-  });
+  function splideProgressBar(slider) {
+    // Set default width of progress bar
+    const progressBar = slider.root.querySelector(".splide__progress-bar");
+    let progressBarWidth = 100 / slider.length;
+    progressBar.style.width = `${progressBarWidth}%`;
 
-  // Refresh on resize
-  splidePrograms.on("resize", function () {
-    splidePrograms.refresh();
-  });
+    // Progress bar on slider moving
+    slider.on("mounted move", function () {
+      var end = slider.Components.Controller.getEnd() + 1;
+      var rate = Math.min((slider.index + 1) / end, 1);
+      progressBar.style.width = String(100 * rate) + "%";
+    });
+
+    // Refresh on resize
+    slider.on("resize", function () {
+      slider.refresh();
+    });
+  }
 
   console.log("DOM fully loaded and parsed");
 });
